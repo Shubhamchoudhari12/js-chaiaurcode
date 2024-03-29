c960a930a1238a900ca6e1aab6b59c19b7fdcc27
Shubham Choudhari <121336273+Shubhamchoudhari12@users.noreply.github.com>
2024-03-29 07:35:22 +0000

    variable and datatype

---
+++ 01_basic/01_one.js
@@ -0,0 +1,4 @@
+const one = 123456;
+let two = "shubham";
+var three = "choudhari";
+console.table([one, two, three])
---
+++ 01_basic/02_two_datatype.js
@@ -0,0 +1,6 @@
+let name ="shubham";
+let number=26;
+let decision=true;
+let answer=null;
+
+console.table([name,number,decision,answer]);
