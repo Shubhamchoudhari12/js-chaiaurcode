de0fa2e54f1ef34fe236cf99df853d2dbe4f60a7
Shubham Choudhari <121336273+Shubhamchoudhari12@users.noreply.github.com>
2024-03-29 11:27:56 +0000

    conversion and operation

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,9 @@
+let score = "26abc";
+// let flag = Number(score);
+console.log(typeof score);
+console.log(typeof (score));
+let flag = Number(score);
+
+console.log(typeof flag);
+
+console.log(flag);
